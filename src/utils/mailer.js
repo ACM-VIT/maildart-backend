24d9947b77c3925b54cb51d2bfce5b0401d287e1
Mahera Furniturewala <82437476+MaheraFurniturewala@users.noreply.github.com>
2021-10-09 21:15:00 +0530

    Create a route for mailgun and initialize the mailgun api (#10)
    * Create a route for mailgun and initialize the mailgun api
    
    * indent code

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,28 +1,28 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
-function concatenatEmails ( emailArray ){
+function concatenatEmails(emailArray) {
     /*
         Iterate over email array and concatenate with previous string
     */
     let emails = ""
     emailArray.forEach((email) => emails += email + ",")
-    return emails 
+    return emails
 }
 
-async function mailer ( recipientEmailArray, subject, text, html ){
+async function mailer(recipientEmailArray, subject, text, html) {
     try {
         // Create a Concatenated String for merging email in a single string
-        let mergedEmail = concatenatEmails (recipientEmailArray)
-        
+        let mergedEmail = concatenatEmails(recipientEmailArray)
+
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
-            user: process.env.EMAIL, // generated ethereal user
-            pass: process.env.PASSWORD, // generated ethereal password
+                user: process.env.EMAIL, // generated ethereal user
+                pass: process.env.PASSWORD, // generated ethereal password
             },
         });
 
@@ -41,4 +41,29 @@ async function mailer ( recipientEmailArray, subject, text, html ){
     }
 }
 
-exports = mailer ; 
+async function mailgun_mailer(recipientEmailArray, subject, text, html) {
+    let mergedEmail = concatenatEmails(recipientEmailArray);
+
+    var API_KEY = process.env.MAILGUN_API_KEY;
+    var DOMAIN = process.env.DOMAIN;
+    var mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
+
+    const data = {
+        from: ` ${process.env.USERNAME}  ${process.env.EMAIL}`,
+        to: mergedEmail,
+        subject: subject,
+        text: text,
+        html: html
+    };
+    try {
+        let info = await mailgun.messages().send(data);
+        console.log("Maessage sent: ", info.body);
+    } catch (error) {
+        console.log("Error in sending mail using mailgun api: ", error);
+    }
+}
+
+module.exports = {
+    mailer,
+    mailgun_mailer
+}
